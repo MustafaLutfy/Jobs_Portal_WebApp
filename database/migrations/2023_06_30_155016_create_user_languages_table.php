50cc9e10e37b462b1030193e57eda2fd4f1c7819
MustafaLutfy <89322808+MustafaLutfy@users.noreply.github.com>
2023-07-01 03:58:57 +0300

    (lang,exp,and company) relationships done

---
+++ database/migrations/2023_06_30_155016_create_user_languages_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_languages', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('lang_id');
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('lang_id')->references('id')->on('languages');
+    
+           
+        });
+    }
+
+  
+    public function down(): void
+    {
+        Schema::dropIfExists('user_skills');
+    }
+};
